c43579ecd7401def6a8d37a1f0d19373419bd488
ShumovAleksej <thefid7262@mail.ru>
2023-07-31 02:54:14 +0500

    clear comment commit

--- handlers/base_handlers.py
+++ handlers/base_handlers.py
@@ -18,15 +18,3 @@ async def process_start_command(message: Message, request: Request, bot: Bot):
 @base_handlers_router.message(Command(commands='commands'))
 async def process_commands_command(message: Message):
     await message.answer(text=LEXICON['/commands'])
-
-# @base_handlers_router.message(Command(commands='test'))
-# async def test(message: Message, bot: Bot, scheduler: AsyncIOScheduler):
-#     await bot.send_message(message.chat.id, 'add job . . .')
-#     scheduler.add_job(apscheduler_def, trigger='date', run_date=datetime.now() + timedelta(seconds=5),
-#                       args=(message, bot))
-# @base_handlers_router.message(Command(commands='both'))
-# async def process_add_user(message: Message, bot: Bot, scheduler: AsyncIOScheduler, request: Request):
-#     await request.add_datauser(message.from_user.id, message.from_user.username)
-#     await bot.send_message(message.chat.id, 'add_user')
-# async def apscheduler_def(message: Message, bot: Bot):
-#     await bot.send_message(message.chat.id, 'complete ! ! !')
